0abe58655fe17a52e97a89cff7cbaa3e73d1f029
Jascha Ephraim <jaschaephraim@gmail.com>
2024-01-03 11:43:59 -0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@
 
 import adapter from '@sveltejs/adapter-auto';
 // eslint-disable-next-line import/no-unresolved
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
