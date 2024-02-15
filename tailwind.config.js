4fe9dc43a92506bc84b87fc639f4899059bf501d
Jascha Ephraim <jaschaephraim@gmail.com>
2024-02-15 13:48:51 -0800

    tailwind defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.svelte'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
